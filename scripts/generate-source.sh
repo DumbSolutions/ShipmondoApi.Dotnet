b963f06f31cb905c6a42db48e0927de537ce95db
Anders K <and3k5@users.noreply.github.com>
2023-06-22 08:53:58 +0200

    Update generate-source script to have same values as csproj

--- scripts/generate-source.sh
+++ scripts/generate-source.sh
@@ -1,8 +1,14 @@
 npx --yes @openapitools/openapi-generator-cli generate -i elements.json \
   -g csharp-netcore --additional-properties=library=restsharp \
-  --additional-properties=packageName=DumbSolutions.ShipmondoApi \
+  --git-host=github.com \
+  --git-user-id=DumbSolutions \
+  --git-repo-id=ShipmondoApi.Dotnet \
   --additional-properties=targetFramework=netstandard2.0 \
   --additional-properties=optionalAssemblyInfo=false \
   "--additional-properties=packageGuid={788F91F2-24A9-4510-8ABE-07C7492245DC}" \
+  --additional-properties=packageTitle=ShipmondoApi.Dotnet \
+  --additional-properties=packageAuthors=DumbSolutions \
+  --additional-properties=packageCompany=DumbSolutions \
+  "--additional-properties=packageDescription='A unofficial thirdparty library for shipmondo api for dotnet'" \
   --additional-properties=packageName=ShipmondoApi.Dotnet && \
   node patch-source-files.js
